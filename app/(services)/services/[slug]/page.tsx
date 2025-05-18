10dc292cf217fcc663b49844375aee78c2d1d275
Matt Rundle <matthewtrundle@gmail.com>
2025-05-18 16:22:42 -0500

    fix blog vercel fix

--- app/(services)/services/[slug]/page.tsx
+++ app/(services)/services/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Metadata } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { getServiceBySlug, getAllServiceSlugs } from '@/lib/data/services';
 
